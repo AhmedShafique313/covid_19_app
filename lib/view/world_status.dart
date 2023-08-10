a9d2c1acdacbf3cfdd6b6c33d3bfd2a35c478f7e
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-08-10 17:22:52 +0500

    container and values

--- lib/view/world_status.dart
+++ lib/view/world_status.dart
@@ -19,6 +19,7 @@ class _WorldStatusScreenState extends State<WorldStatusScreen>
     _controller.dispose();
   }
 
+  // ignore: non_constant_identifier_names
   final ColorList = <Color>[Colors.green, Colors.blue, Colors.red];
 
   @override
@@ -26,7 +27,7 @@ class _WorldStatusScreenState extends State<WorldStatusScreen>
     return Scaffold(
       body: SafeArea(
           child: Padding(
-        padding: EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -39,15 +40,74 @@ class _WorldStatusScreenState extends State<WorldStatusScreen>
                   height: 200,
                   width: 300,
                   child: PieChart(
+                    animationDuration: const Duration(milliseconds: 1200),
                     chartType: ChartType.ring,
                     chartRadius: 150,
                     dataMap: const {"Total": 70, "Active": 21, "Deaths": 9},
                     colorList: ColorList,
+                    legendOptions: const LegendOptions(
+                        legendPosition: LegendPosition.left),
                   )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 600),
+              child: Card(
+                child: Column(
+                  children: [
+                    ResusableRow(title: "Title", value: "300"),
+                    ResusableRow(title: "Title", value: "300"),
+                    ResusableRow(title: "Title", value: "300"),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              // ignore: sort_child_properties_last
+              child: const Center(
+                  child: Text(
+                'Track Countries',
+                style: TextStyle(fontSize: 16),
+              )),
+              height: 50,
+              width: 150,
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(10)),
             )
           ],
         ),
       )),
+    );
+  }
+}
+
+class ResusableRow extends StatelessWidget {
+  String title, value;
+  ResusableRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title),
+                Text(value),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
